76706e56f2d7d3b300844aa60bf0cd197fe7885f
Kevin J Medina <kevin@Kevins-MacBook-Air-5.local>
2026-06-04 14:02:45 -0400

    Added typing to the implementations of Stack and Queue and added error handling for the case of an empty stack or empty queue.

--- queues/impl_queues.py
+++ queues/impl_queues.py
@@ -1,22 +1,27 @@
 from collections import deque
+from typing import Any
 
 class Queue:
-    def __init__(self):
+    def __init__(self) -> None:
         self.queue = deque()
 
-    def enqueue(self,item):
+    def enqueue(self,item: Any):
         self.queue.append(item)
 
-    def dequeue(self):
+    def dequeue(self) -> Any:
+        if self.is_empty():
+            raise IndexError('Queue is empty')
         return self.queue.popleft()
 
-    def peek(self):
+    def peek(self) -> Any:
+        if self.is_empty():
+            raise IndexError('Queue is empty')
         return self.queue[0]
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.queue) == 0
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.queue)
 
-    
+
--- stacks/impl_stacks.py
+++ stacks/impl_stacks.py
@@ -1,27 +1,27 @@
 from collections import deque
+from typing import Any
+
 
 class Stack:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.stack = deque()
 
-    def push(self, value):
+    def push(self, value: Any):
         self.stack.append(value)
 
-    def pop(self):
-        try:
-            self.stack.pop()
-        except IndexError:
-            print("Stack is empty")
-            return None
+    def pop(self) -> Any:
+        if self.is_empty():
+            raise IndexError('Stack is empty')
+        return self.stack.pop()
 
-    def peek(self):
+    def peek(self) -> Any:
         return self.stack[-1]
 
-    def is_empty(self):
+    def is_empty(self) -> bool:
         return len(self.stack) == 0
 
-    def __len__(self):
+    def __len__(self) -> int:
         return len(self.stack)
 
 
